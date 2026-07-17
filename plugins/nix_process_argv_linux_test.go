509a531524ef35e0ddc32fb4ecdbdbba7ebed423
Jess Sullivan <Jess@sulliwood.org>
2026-07-17 14:24:37 -0400

    test(nix): make Linux argv probe hermetic (TIN-2676) (#122)

--- plugins/nix_process_argv_linux_test.go
+++ plugins/nix_process_argv_linux_test.go
@@ -5,10 +5,8 @@ package plugins
 import (
 	"errors"
 	"os"
-	"os/exec"
 	"reflect"
 	"testing"
-	"time"
 )
 
 func TestDecodeNixLinuxProcessArgvPreservesBoundaries(t *testing.T) {
@@ -21,37 +19,12 @@ func TestDecodeNixLinuxProcessArgvPreservesBoundaries(t *testing.T) {
 }
 
 func TestNixProcessArgvLinux(t *testing.T) {
-	const helperEnvironment = "TINYLAND_NIX_ARGV_LINUX_HELPER"
-	if os.Getenv(helperEnvironment) == "1" {
-		for {
-			time.Sleep(time.Hour)
-		}
-	}
-
-	arguments := []string{
-		"-test.run=^TestNixProcessArgvLinux$",
-		"--",
-		"",
-		`"literal quotes"`,
-		"argument with spaces",
-		"",
-	}
-	command := exec.Command(os.Args[0], arguments...)
-	command.Env = append(os.Environ(), helperEnvironment+"=1")
-	if err := command.Start(); err != nil {
-		t.Fatalf("start helper process: %v", err)
-	}
-	t.Cleanup(func() {
-		_ = command.Process.Kill()
-		_ = command.Wait()
-	})
-
-	got, err := nixProcessArgv(command.Process.Pid)
+	got, err := nixProcessArgv(os.Getpid())
 	if err != nil {
 		t.Fatalf("nixProcessArgv returned error: %v", err)
 	}
-	if !reflect.DeepEqual(got, command.Args) {
-		t.Fatalf("nixProcessArgv() = %#v, want %#v", got, command.Args)
+	if !reflect.DeepEqual(got, os.Args) {
+		t.Fatalf("nixProcessArgv() = %#v, want %#v", got, os.Args)
 	}
 }
 
